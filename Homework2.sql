7818f38d156739c376c98181872b1377f17d1f6d
SkrypKate <47118485+SkrypKate@users.noreply.github.com>
2019-02-04 00:48:37 +0300

    Update Homework2.sql

--- Homework2.sql
+++ Homework2.sql
@@ -1,10 +1,10 @@
 SELECT 'ФИО: Скрыпникова Екатерина';
 -- 1. Простые выборки
--- 1.1 
+-- 1.1 выбрать 10 записей из таблицы ratings
 SELECT * 
 FROM ratings 
 LIMIT 10;
--- 1.2
+-- 1.2 выбрать из таблицы links всё записи, у которых imdbid оканчивается на "42", а поле movieid между 100 и 1000
 SELECT *
 FROM links
 WHERE
@@ -12,7 +12,7 @@ imdbid like '%42'
 AND movieid BETWEEN 100 AND 1000
 LIMIT 10;
 -- 2. Сложные выборки: JOIN
--- 2.1
+-- 2.1 выбрать из таблицы links все imdbId, которым ставили рейтинг 5
 SELECT *
 FROM links
 JOIN ratings
@@ -20,4 +20,6 @@ ON links.movieid=ratings.movieid
 WHERE ratings.rating = 5
 LIMIT 10;
 -- 3. Агрегация данных: базовые статистики
--- 3.1
+-- 3.1 Посчитать число фильмов без оценок
+SELECT
+COUNT (DISTINCT movieid) as movieNull
