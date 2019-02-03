cd610b443d980fd51fe7b87775c2521470d4021a
SkrypKate <47118485+SkrypKate@users.noreply.github.com>
2019-02-04 00:20:17 +0300

    Update Homework2.sql

--- Homework2.sql
+++ Homework2.sql
@@ -1,6 +1,6 @@
 SELECT 'ФИО: Скрыпникова Екатерина';
--- 1.Простые выборки
---1.1 
+-- 1. Простые выборки
+-- 1.1 
 SELECT * 
 FROM ratings 
 LIMIT 10;
@@ -8,7 +8,16 @@ LIMIT 10;
 SELECT *
 FROM links
 WHERE
-imdbit like '%42'
-AND movieid >100
-AND movieid <1000
+imdbid like '%42'
+AND movieid BETWEEN 100 AND 1000
 LIMIT 10;
+-- 2. Сложные выборки: JOIN
+-- 2.1
+SELECT *
+FROM links
+JOIN ratings
+ON links.movieid=ratings.movieid
+WHERE ratings.rating = 5
+LIMIT 10;
+-- 3. Агрегация данных: базовые статистики
+-- 3.1
