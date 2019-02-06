4dde01404cf0b451d529553fd0ccbac56d42221e
SkrypKate <47118485+SkrypKate@users.noreply.github.com>
2019-02-06 23:54:15 +0300

    Update Homework3.sql

--- Homework3.sql
+++ Homework3.sql
@@ -3,7 +3,8 @@ SELECT 'ФИО: Скрыпникова Екатерина';
 -- Оконные функции
 SELECT
   userId, movieId, rating,
-  (rating - MIN(rating) OVER (PARTITION BY userId))/(MAX(rating) OVER (PARTITION BY userId) - MIN(rating) OVER (PARTITION BY userId)) as normed_rating,
+  (rating - MIN(rating) OVER (PARTITION BY userId))/(MAX(rating) OVER (PARTITION BY userId) - MIN(rating) OVER
+(PARTITION BY userId)) as normed_rating,
   AVG(rating) OVER (PARTITION BY userId) as avg_rating
 FROM (
     SELECT DISTINCT
@@ -14,4 +15,18 @@ FROM (
 
 LIMIT 30;
 
+-- ETL
+-- Extract
+-- Проверьте, что в директории data присутствует файл с ключевыми словами по фильмам
 
+netology@NetologyAD-2:~$  ls $NETOLOGY_DATA/raw_data | grep keywords
+                                                                                                                
+-- zahodim pod postgres
+
+ netology@NetologyAD-2:~$ sudo su -l postgres
+
+ -- vipolnyem komandu sozdat table cherez PostgreSQL
+ postgres@NetologyAD-2:~$ psql -U postgres -c "CREATE TABLE IF NOT EXISTS keywords (id SERIAL, tags VARCHAR(355));"
+
+ -- копирования данных из файла в созданную вами таблицу
+  postgres@NetologyAD-2:~$ psql -U postgres -c "copy keywords FROM '/home/netology
