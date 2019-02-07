dac39bfa2096d9f7fb6dc24babc5e7cbb363d4c0
SkrypKate <47118485+SkrypKate@users.noreply.github.com>
2019-02-07 23:48:43 +0300

    Update Homework3.sql

--- Homework3.sql
+++ Homework3.sql
@@ -12,25 +12,43 @@ FROM (
     FROM ratings
     WHERE userId <>1 LIMIT 1000
 ) as sample
-
+GROUP BY userid
 LIMIT 30;
 
 -- ETL
 -- Extract
 -- Проверьте, что в директории data присутствует файл с ключевыми словами по фильмам
-
 netology@NetologyAD-2:~$  ls $NETOLOGY_DATA/raw_data | grep keywords
-                                                                                                                
--- zahodim pod postgres
 
- netology@NetologyAD-2:~$ sudo su -l postgres
+-- Заходим под пользователем postgres
+netology@NetologyAD-2:~$ sudo su -l postgres
 
- -- vipolnyem komandu sozdat table cherez PostgreSQL
- postgres@NetologyAD-2:~$ psql -U postgres -c "CREATE TABLE IF NOT EXISTS keywords_3 (id SERIAL, tags TEXT);"
+-- Выполняем команду по созданию таблицы через PostgreSQL
+postgres@NetologyAD-2:~$ psql -U postgres -c "CREATE TABLE IF NOT EXISTS keywords (id SERIAL, tags TEXT);"
 
- -- копирования данных из файла в созданную вами таблицу
-  postgres@NetologyAD-2:~$ psql -U postgres -c "copy keywords FROM '/usr/local/share/netology/raw_data/keywords.csv' delimiter ',' CSV HEADER"
+-- Копируем данные из файла в созданную таблицу
+postgres@NetologyAD-2:~$ psql -U postgres -c "copy keywords FROM '/usr/local/share/netology/raw_data/keywords.csv' delimiter ',' CSV HEADER"
 
---Проверьте, что в таблице есть записи
+-- Проверяем, что в таблице есть записи
+SELECT COUNT(*) FROM keywords;
 
-SELECT  COUNT(*) FROM keywords_3;
+-- Transform
+-- Запрос 1,2
+WITH top_rated as (
+ SELECT
+ movieid, 
+ AVG(rating) OVER (PARTITION BY userId) as avg_rating
+ FROM public.ratings
+ GROUP BY movieid 
+ HAVING COUNT(userid) > 50 
+ ORDER BY movieid ASC, avg_rating DESC
+ LIMIT 150
+)
+ JOIN keywords
+ ON top_rated.movieid=keywords.movieid
+ LIMIT 20;
+ 
+ 
+ 
+ 
+ 
